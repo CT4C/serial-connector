f0967c31b0312ee395981bae0e856f16ecfc9af6
edpryk <edpryk@gmail.com>
2023-07-27 18:41:36 +0300

    working on setup

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ from distutils.core import setup
 
 setup(
     name="sdebug",
-    version="1.0",
+    version="0.0.1",
     author="Dmytro Prykhodko",
-    author_email="edpryk@gmaill.com.main",
+    author_email="edpryk@gmail.com",
     packages=["serial_debug"],
+    keywords=["serial", "debug", "communication"],
+    download_url="https://github.com/edpryk/serial-debug",
+    package_dir={"serial_debug": "src/serial_debug"},
     description="Read serial port and write output either to the `serial_output.txt` or `stdout` User can write data to a serial port through typing it to the `stdin`"
 )
